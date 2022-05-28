076a5a65c2ebc7f9a92b1503161513eb3345b86d
Guilherme Vitor Neves Pereira <96185124+Guitor-92@users.noreply.github.com>
2022-05-28 00:13:02 -0300

    Update IRepositorio.cs

--- Interfaces/IRepositorio.cs
+++ Interfaces/IRepositorio.cs
@@ -4,11 +4,11 @@ namespace DIO.Series.Interfaces
 {
     public interface IRepositorio<T>
     {
-         List<T> Lista();
-         T RetornaPorId(int id);
-         void Insere(T entidade);
-         void Exclui(int id);
-         void Atualiza(int id, T entidade);
-         int ProximoId();
+        List<T> Lista();
+        T RetornaPorId(int id);        
+        void Insere(T entidade);        
+        void Exclui(int id);        
+        void Atualiza(int id, T entidade);
+        int ProximoId();
     }
 }
