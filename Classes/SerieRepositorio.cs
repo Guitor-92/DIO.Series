b40ca129081cdac099f448fedf228125932bae71
Guilherme Vitor Neves Pereira <96185124+Guitor-92@users.noreply.github.com>
2022-05-28 00:11:17 -0300

    Update SerieRepositorio.cs

--- Classes/SerieRepositorio.cs
+++ Classes/SerieRepositorio.cs
@@ -4,10 +4,10 @@ using DIO.Series.Interfaces;
 
 namespace DIO.Series
 {
-    public class SerieRepositorio : IRepositorio<Serie>
-    {
+	public class SerieRepositorio : IRepositorio<Serie>
+	{
         private List<Serie> listaSerie = new List<Serie>();
-        public void Atualiza(int id, Serie objeto)
+		public void Atualiza(int id, Serie objeto)
 		{
 			listaSerie[id] = objeto;
 		}
@@ -36,6 +36,5 @@ namespace DIO.Series
 		{
 			return listaSerie[id];
 		}
-        
-    }
+	}
 }
